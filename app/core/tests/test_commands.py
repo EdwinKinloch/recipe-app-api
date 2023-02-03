0695f6c2bbb0e7158494bc61c2a20a72a0f92837
ekinloch <edwin.kinloch@infor.com>
2023-02-03 14:06:31 -0600

    Corrected spelling errors

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,7 @@ Unit Test for testing the wait_for_db code
 
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -15,7 +15,8 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
-    def test_wait_for_db_ready(self, patch_check):
+    def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
-        patch_check.return_value = True
+        patched_check.return_value = True
         call_command('wait_for_db')
+        patched_check.assert_called_once_with(database=['default'])
