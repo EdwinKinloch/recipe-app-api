62fe0787027456324175b0e7554006f80788bacf
Edwin <49170990+EdwinKinloch@users.noreply.github.com>
2023-02-05 22:22:07 -0600

    Corrected typing errors
    Added Docstring, removed spaces and corrected typing errors

--- app/core/models.py
+++ app/core/models.py
@@ -1,12 +1,12 @@
+"""
+Database models.
+"""
 from django.db import models
 from django.contrib.auth.models import (
   AbstractBaseUser,
   BaseUserManager,
   PermissionsMixin,
 )
-
-
-
 
 
 class UserManager(BaseUserManager):
@@ -21,14 +21,13 @@ class UserManager(BaseUserManager):
     return user
 
 
-
 class User(AbstractBaseUser, PermissionsMixin):
-  """ User in the system"""
+  """ User in the systems"""
   email = models.EmailField(max_length=255, unique=True)
   name = models.CharField(max_length=255)
   is_active = models.BooleanField(default=True)
   is_staff = models.BooleanField(default=False)
 
-  object = UserManager()
+  objects = UserManager()
 
   USERNAME_FIELD = 'email'
