edbca4fde1b80894d2fee3b1d69b267e1dbb13de
ekinloch <edwin.kinloch@infor.com>
2023-02-10 14:53:04 -0600

    Correct invalid email on line 72

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -38,7 +38,7 @@ class PublicUserApiTests(TestCase):
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
-    def test_user_with_email_exist_error(self):
+    def test_user_with_email_exists_error(self):
         """Test error returned if user with email already exists."""
         payload = {
             'email': 'test@example.com',
@@ -55,21 +55,21 @@ class PublicUserApiTests(TestCase):
         payload = {
             'email': 'test@example.com',
             'password': 'pw',
-            'name': 'Test Name',
+            'name': 'Test name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exist = get_user_model().objects.filter(
+        user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
-        self.assertFalse(user_exist)
+        self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
         """Test generate token for valid credentials."""
         user_detials = {
             'name': 'Test Name',
-            'email': 'test@example,com',
+            'email': 'test@example.com',
             'password': 'test-user-password123',
         }
         create_user(**user_detials)
