7ebe6bfb6e7fe0ef6e09afaf668d841486b79b76
ekinloch <edwin.kinloch@infor.com>
2023-02-10 20:57:44 -0600

    Added brackets

--- app/user/views.py
+++ app/user/views.py
@@ -24,8 +24,8 @@ class CreateTokenView(ObtainAuthToken):
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manager the authenticated user."""
     serializer_class = UserSerializer
-    authentication_classes = (authentication.TokenAuthentication)
-    permission_classes = (permissions.IsAuthenticated)
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         """Retrieve and return authentication user."""
