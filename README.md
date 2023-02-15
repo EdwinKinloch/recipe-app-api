5c3018a58dee54047b5b23074032c04d875faea6
Edwin <49170990+EdwinKinloch@users.noreply.github.com>
2023-02-15 06:36:03 -0600

    Recipe api (#3)
    * registered the new Recipe model
    
    * Created a new model for the recipe app
    
    * Migration file
    
    * Created a new test for recipe model
    
    * Updated to better explain project
    
    * enabled new recipe app
    
    * Initial file upload from creating new recipe app
    
    * Created a tests for recipe api
    
    * Initial file upload for recipe serializers
    
    * Initial upload for the views file
    
    * Initial file upload
    
    * Added recipe url path
    
    * Testing recipe detail API
    Added a new import and test method
    
    * Serializer for detail recipe API
    Added a new class and class meta for gathering the description.
    
    * Added new serializer methods
    Made an edit the serializer_class variable and two new methods.
    
    * Testing creating recipe API
    Added a new test creating a recipe by using a custom payload.
    
    * New method for creating a new recipe
    
    * Addtional test for recipe api
    
    * Corrected errors found in Flake8
    
    ---------
    
    Co-authored-by: ekinloch <edwin.kinloch@infor.com>

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # recipe-app-api
 Recipe API project
-This personal project displays my skills on building a backend REST API with Python and Django 
+Advanced Backend REST API Development using Python, Django (3.2), Django REST Framework (3.12), Docker, GitHub Actions, Postgres, and Test Driven Development.
+
+Developed knowledge and practical skills in building advanced APIs, including user authentication, creating and updating user profiles, changing passwords, filtering and searching objects, uploading images, and more.
+
+Constructed a fully functioning recipe API that allows for uploading and storing recipes with titles, price points, cooking times, ingredients, and tags.
+
+Utilized Docker and Docker-Compose for project setup, GitHub Actions for automatic linting and unit tests, and the Django Test Framework for writing unit tests.
+
+Implemented best practices such as Test Driven Development, handling media file uploads with Django, customizing the Django admin, and configuring a Postgres database.
--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Database models.
 """
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
   AbstractBaseUser,
@@ -10,35 +11,52 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-  """Create, save and return a new user."""
-
-  def create_user(self, email, password=None, **extra_fields):
     """Create, save and return a new user."""
-    if not email:
-      raise ValueError('Users must have an email address.')
-    user = self.model(email=self.normalize_email(email), **extra_fields)
-    user.set_password(password)
-    user.save(using=self._db)
 
-    return user
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('Users must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-  def create_superuser(self, email, password):
-      """Create and return a new super."""
-      user = self.create_user(email, password)
-      user.is_staff = True
-      user.is_superuser = True
-      user.save(using=self._db)
+        return user
 
-      return user
+    def create_superuser(self, email, password):
+        """Create and return a new super."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-  """ User in the systems"""
-  email = models.EmailField(max_length=255, unique=True)
-  name = models.CharField(max_length=255)
-  is_active = models.BooleanField(default=True)
-  is_staff = models.BooleanField(default=False)
+    """ User in the systems"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-  objects = UserManager()
+    objects = UserManager()
 
-  USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.title
