23900fd3486a91bbcb5e6d9f348a8b4df66aac86
ekinloch <edwin.kinloch@infor.com>
2023-02-20 10:19:15 -0600

    Added a new Tag Serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,7 +3,10 @@ Serializers for the recipe APIs
 """
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -20,4 +23,13 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+        read_only_fields = ['id']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
         read_only_fields = ['id']
