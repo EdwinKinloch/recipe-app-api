6631fef6a1009f623ddf2d75cfbe760e27da187d
ekinloch <edwin.kinloch@infor.com>
2023-02-20 07:39:25 -0600

    New testing for recipe tag

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,6 +9,13 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Helper function to create a user."""
+    return get_user_model().objects.create_user(
+      email=email,
+      password=password,
+    )
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -69,3 +76,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_recipe_tag(self):
+        """Test creating a recipe tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
